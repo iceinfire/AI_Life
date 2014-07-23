346a158cd7a16375febc8e81230c1042c4a6a24a
TartPanther47 <cfmgp@hotmail.com>
2014-07-23 15:46:23 -0400

    Graph and stuff added...

---
+++ src/net/iCEiNFiRE/AI_Life/GameWorld.java
@@ -0,0 +1,11 @@
+package net.iCEiNFiRE.AI_Life;
+
+public class GameWorld {
+	
+	public void update() {
+		
+	}
+	public void render() {
+		Graph.drawPlayer(100, 100);
+	}
+}
---
+++ src/net/iCEiNFiRE/AI_Life/Graph.java
@@ -0,0 +1,37 @@
+package net.iCEiNFiRE.AI_Life;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+import javax.swing.JFrame;
+
+public class Graph {
+	public static JFrame frame;
+	public static Graphics g;
+	public static BufferStrategy bs;
+	public static void init() {
+		frame = new JFrame("Life is Amazing!");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800, 600);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		frame.createBufferStrategy(2);
+	}
+	public static void pullPixels() {
+		bs = frame.getBufferStrategy();
+		g = bs.getDrawGraphics();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, frame.getWidth(), frame.getHeight());
+		g.setColor(Color.WHITE);
+	}
+	public static void pushPixels() {
+		g.dispose();
+		bs.show();
+		frame.update(g);
+	}
+	public static void drawPlayer(int x, int y) {
+		g.setColor(Color.ORANGE);
+		g.fillOval(x, y, 10, 10);
+	}
+}
